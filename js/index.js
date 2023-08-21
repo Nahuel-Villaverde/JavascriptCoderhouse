c2c352291ae452e7da858efb2c71241ca2e823e1
Nahuel Villaverde <nahuelvillaverdeguzman@gmail.com>
2023-08-21 15:48:33 -0300

    modificar edad

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
+// Definir constructor de Persona
+class Persona {
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+
+  modificarEdad(nuevaEdad) {
+    this.edad = nuevaEdad;
+  }
+}
+
 const personas = [
-  { nombre: "Ana", edad: 25 },
-  { nombre: "Juan", edad: 30 },
-  { nombre: "María", edad: 28 },
-  { nombre: "Carlos", edad: 22 }
+  new Persona("Ana", 25),
+  new Persona("Juan", 30),
+  new Persona("María", 28),
+  new Persona("Carlos", 22)
 ];
 
 const filtrarMayores = (edadMinima) => {
@@ -19,24 +31,24 @@ for (const persona of personas) {
   listaPersonas += `${persona.nombre} (Edad: ${persona.edad})\n`;
 }
 
-alert(`Bienvenido al filtro de personas, podrás crear una persona, filtrar por edad y buscar un nombre. Lista de personas:\n${listaPersonas}`);
+alert(`Bienvenido al filtro de personas, podrás crear una persona, filtrar por edad, buscar un nombre y modificar la edad de la persona que quieras. Lista de personas:\n${listaPersonas}`);
 
 const nuevoNombre = prompt("Ingresa el nombre de la nueva persona:");
 let nuevaEdad = parseInt(prompt("Ingresa la edad de la nueva persona:"));
 
 while (isNaN(nuevaEdad)) {
-  nuevaEdad = parseInt(prompt("No ingresó un numero, ingresa la edad de la nueva persona:"));
+  nuevaEdad = parseInt(prompt("No ingresó un número, ingresa la edad de la nueva persona:"));
 }
 
-personas.push({ nombre: nuevoNombre, edad: nuevaEdad });
+personas.push(new Persona(nuevoNombre, nuevaEdad));
 
 let seguirFiltrando = true;
 
 while (seguirFiltrando) {
 
-  let personasMayoresDe = parseInt(prompt("Ingresa la edad minima que debe tener la gente para su filtro"));
+  let personasMayoresDe = parseInt(prompt("Ingresa la edad mínima que debe tener la gente para su filtro"));
   while (isNaN(personasMayoresDe)) {
-    personasMayoresDe = parseInt(prompt("No ingresó un numero, ingresa la edad minima que debe tener la gente para su filtro"));
+    personasMayoresDe = parseInt(prompt("No ingresó un número, ingresa la edad mínima que debe tener la gente para su filtro"));
   }
 
   const filtroMayoresAplicado = filtrarMayores(personasMayoresDe);
@@ -47,7 +59,6 @@ while (seguirFiltrando) {
   const nombreBuscado = prompt("Ingresa el nombre de la persona que deseas buscar:");
   let personaBuscada = buscarPersonaPorNombre(nombreBuscado);
 
-
   if (personaBuscada) {
     personaBuscada = `${personaBuscada.nombre} (Edad: ${personaBuscada.edad})`;
   } else {
@@ -57,7 +68,60 @@ while (seguirFiltrando) {
   alert("Persona buscada:\n" + personaBuscada);
 
   const continuarFiltrando = confirm("¿Quieres realizar otro filtro?");
-  if (!continuarFiltrando) {
-    seguirFiltrando = false;
-  }
+  seguirFiltrando = continuarFiltrando;
 }
+
+let seguirModificando = true;
+let listaPersonasVieja = "";
+
+for (const persona of personas) {
+  listaPersonasVieja += `${persona.nombre} (Edad: ${persona.edad})\n`;
+}
+
+console.log(`Lista de personas:\n${listaPersonasVieja}`);
+
+
+while (seguirModificando) {
+  const nombreModificado = prompt("Ingresa el nombre de la persona a modificar:");
+  const personaAModificar = buscarPersonaPorNombre(nombreModificado);
+
+  if (personaAModificar) {
+    let nuevaEdad = parseInt(prompt(`Ingresa la nueva edad para ${personaAModificar.nombre}:`));
+
+    while (isNaN(nuevaEdad)) {
+      nuevaEdad = parseInt(prompt("No ingresó un número, ingresa la nueva edad:"));
+    }
+
+    personaAModificar.modificarEdad(nuevaEdad);
+    alert(`Edad de ${personaAModificar.nombre} modificada a ${nuevaEdad} años.`);
+
+  } else {
+    alert("No se encontró a la persona a modificar.");
+    seguirModificando = false;
+  }
+
+  const continuarModificando = confirm("¿Quieres seguir modificando?");
+  seguirModificando = continuarModificando;
+}
+
+let listaPersonasNueva = "";
+
+for (const persona of personas) {
+  listaPersonasNueva += `${persona.nombre} (Edad: ${persona.edad})\n`;
+}
+
+console.log(`Lista de personas:\n${listaPersonasNueva}`);
+
+const fechaActual = new Date();
+console.log("Fecha actual:", fechaActual);
+
+const anio = fechaActual.getFullYear();
+console.log("Año actual:", anio);
+
+const mes = fechaActual.getMonth() + 1;
+console.log("Mes actual:", mes);
+
+const dia = fechaActual.getDate();
+console.log("Día actual:", dia);
+
+alert("Fin");
