6d6c121cf6082b456f2e80d11e56b705c5c0fef4
Nahuel Villaverde <nahuelvillaverdeguzman@gmail.com>
2023-09-04 21:08:13 -0300

    carritoLocalStorage

---
+++ js/cart.js
@@ -0,0 +1,156 @@
+const modalContainer = document.getElementById("modal-container");
+const modalOverlay = document.getElementById("modal-overlay");
+
+const cartBtn = document.getElementById("cart-btn");
+const cartCounter = document.getElementById("cart-counter");
+
+const displayCart = () => {
+    modalContainer.innerHTML = "";
+
+    modalOverlay.style.display = "block";
+    modalContainer.style.display = "block";
+
+    //Modal Header
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+
+    const modalClose = document.createElement("div");
+    modalClose.innerText = "❌";
+    modalClose.className = "modal-close";
+    modalHeader.append(modalClose);
+
+    modalClose.addEventListener("click", ()=> {
+        modalContainer.style.display = "none";
+        modalOverlay.style.display = "none";
+    })
+    const modalTitle = document.createElement("div");
+    modalTitle.innerText = "TU CARRITO";
+    modalTitle.className = "modal_title";
+    modalHeader.append(modalTitle);
+
+    modalContainer.append(modalHeader);
+
+
+    //Modal Body
+    if(cart.length > 0){
+        cart.forEach((product) => {
+            const modalBody = document.createElement("div");
+            modalBody.className = "modal-body";
+            modalBody.innerHTML = `
+            <div class="product">
+                <img class="product-img" src="${product.img}" />
+                <div class="product-info">
+                    <h4>${product.productName}</h4>
+                </div>
+                <div class="quantity">
+                    <span class="quantity-btn-decrese">-</span>
+                    <span class="quantity-input">${product.quantity}</span>
+                    <span class="quantity-btn-increse">+</span>
+                </div>
+                <div class="price">$${product.price * product.quantity}</div>
+                <div class="delete-product">❌</div>
+            `;
+            modalContainer.append(modalBody);
+
+            const decrese = modalBody.querySelector(".quantity-btn-decrese");
+            decrese.addEventListener("click", ()=>{
+                if(product.quantity !== 1){
+                    product.quantity--;
+                    displayCart();
+                    saveLocal();
+                    displayCartCounter();
+                }
+            });
+
+            const increse = modalBody.querySelector(".quantity-btn-increse");
+            increse.addEventListener("click", ()=>{
+                    product.quantity++;
+                    displayCart();
+                    saveLocal();
+                    displayCartCounter();
+            });
+
+            //delete 
+            const deleteProduct = modalBody.querySelector(".delete-product");
+            deleteProduct.addEventListener("click", ()=>{
+                deleteCartProduct(product.id)
+                
+            })
+        });
+
+        //Modal Footer
+        const total = cart.reduce((acumulador, elemento) => acumulador + elemento.price* elemento.quantity, 0);
+
+        const modalFooter = document.createElement("div");
+        modalFooter.className = "modal-footer";
+        modalFooter.innerHTML = `
+            <div class = "linea">
+                <div></div>
+            </div>
+            <div class = "total-price">TOTAL: ${total}</div>
+            <button id="mostrarTick" class="tick-button">COMPRAR</button>
+        `;
+        modalContainer.append(modalFooter);
+
+        const tick = document.getElementById('mostrarTick')  
+        
+        const tickOverlay = document.getElementById("tick-overlay");
+        const tickImg = document.getElementById("tick-img");
+
+        tick.addEventListener('click', () => {
+            // Limpia el contenido del carrito (borra el array cart)
+            cart.length = 0;
+            displayCart();
+            displayCartCounter();
+          
+            // Cierra el modal
+            modalContainer.style.display = "none";
+            modalOverlay.style.display = "none";
+          
+            // Muestra el tick (esto ya está en tu código)
+            tickOverlay.style.display = "block";
+            tickImg.style.display = "block";
+          
+            // Configura un temporizador para ocultar el tick después de 1 segundo
+            setTimeout(function () {
+              tickOverlay.style.display = "none";
+              tickImg.style.display = "none";
+            }, 1000);
+
+            saveLocal();
+          });
+
+
+    }else{
+        const modalText = document.createElement("h2");
+        modalText.className = "modal-vacio";
+        modalText.innerText = "TU CARRITO ESTA VACIO";
+        modalContainer.append(modalText);
+    }
+};
+
+cartBtn.addEventListener("click", displayCart);
+
+const deleteCartProduct = (id) => {
+    const foundId = cart.findIndex((elemento) => elemento.id === id);
+    console.log(foundId);
+    cart.splice(foundId, 1);
+    displayCart();
+    saveLocal();
+    displayCartCounter();
+};
+
+const displayCartCounter = () => {
+    const cartLength = cart.reduce((acumulador, elemento) => acumulador +elemento.quantity, 0);
+    localStorage.setItem("cartLength", JSON.stringify(cartLength))
+    if(cartLength > 0){
+        cartCounter.style.display = "block";
+        cartCounter.innerText = JSON.parse(localStorage.getItem("cartLength"));
+    }else{
+        cartCounter.style.display = "none";
+    }
+}
+
+displayCartCounter();
+
+
--- js/index.js
+++ js/index.js
@@ -1,127 +1,55 @@
-// Definir constructor de Persona
-class Persona {
-  constructor(nombre, edad) {
-    this.nombre = nombre;
-    this.edad = edad;
-  }
+const shopContent = document.getElementById("shopContent");
 
-  modificarEdad(nuevaEdad) {
-    this.edad = nuevaEdad;
-  }
+//get item
+const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+productos.forEach((product)=>{
+    const content = document.createElement("div");
+    content.className = "moto"
+    content.innerHTML = `
+    <img src = "${product.img}">  
+    <h3>${product.productName}</h3>
+    <p class = "precio">$${product.price}</p>
+    `
+    shopContent.append(content);
+
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Agregar"
+    buyButton.className = 'agregar-btn';
+
+    content.append(buyButton)
+
+    buyButton.addEventListener("click", () => {
+      const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+      if(repeat){
+        cart.map((prod)=> {
+          if(prod.id === product.id){
+            prod.quantity++;
+            displayCartCounter();
+            saveLocal();
+          }
+        });
+      }else {
+        cart.push({
+          id: product.id,
+          productName: product.productName,
+          price: product.price,
+          quantity: product.quantity,
+          img: product.img,
+        });
+        displayCartCounter();
+        saveLocal();
+      }
+      console.log(cart);
+    })
+});
+
+//set item
+const saveLocal = () => {
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-const personas = [
-  new Persona("Ana", 25),
-  new Persona("Juan", 30),
-  new Persona("María", 28),
-  new Persona("Carlos", 22)
-];
-
-const filtrarMayores = (edadMinima) => {
-  return personas.filter(persona => persona.edad >= edadMinima);
-};
-
-function buscarPersonaPorNombre(nombre) {
-  return personas.find(persona => persona.nombre === nombre);
-}
-
-let listaPersonas = "";
-
-for (const persona of personas) {
-  listaPersonas += `${persona.nombre} (Edad: ${persona.edad})\n`;
-}
-
-alert(`Bienvenido al filtro de personas, podrás crear una persona, filtrar por edad, buscar un nombre y modificar la edad de la persona que quieras. Lista de personas:\n${listaPersonas}`);
-
-const nuevoNombre = prompt("Ingresa el nombre de la nueva persona:");
-let nuevaEdad = parseInt(prompt("Ingresa la edad de la nueva persona:"));
-
-while (isNaN(nuevaEdad)) {
-  nuevaEdad = parseInt(prompt("No ingresó un número, ingresa la edad de la nueva persona:"));
-}
-
-personas.push(new Persona(nuevoNombre, nuevaEdad));
-
-let seguirFiltrando = true;
-
-while (seguirFiltrando) {
-
-  let personasMayoresDe = parseInt(prompt("Ingresa la edad mínima que debe tener la gente para su filtro"));
-  while (isNaN(personasMayoresDe)) {
-    personasMayoresDe = parseInt(prompt("No ingresó un número, ingresa la edad mínima que debe tener la gente para su filtro"));
-  }
-
-  const filtroMayoresAplicado = filtrarMayores(personasMayoresDe);
-  const filtroMayoresAplicadoString = filtroMayoresAplicado.map(persona => `${persona.nombre} (Edad: ${persona.edad})`).join('\n');
-
-  alert("Personas mayores de " + personasMayoresDe + " años:\n" + filtroMayoresAplicadoString);
-
-  const nombreBuscado = prompt("Ingresa el nombre de la persona que deseas buscar:");
-  let personaBuscada = buscarPersonaPorNombre(nombreBuscado);
-
-  if (personaBuscada) {
-    personaBuscada = `${personaBuscada.nombre} (Edad: ${personaBuscada.edad})`;
-  } else {
-    personaBuscada = "No se encontró a la persona buscada";
-  }
-
-  alert("Persona buscada:\n" + personaBuscada);
-
-  const continuarFiltrando = confirm("¿Quieres realizar otro filtro?");
-  seguirFiltrando = continuarFiltrando;
-}
-
-let seguirModificando = true;
-let listaPersonasVieja = "";
-
-for (const persona of personas) {
-  listaPersonasVieja += `${persona.nombre} (Edad: ${persona.edad})\n`;
-}
-
-console.log(`Lista de personas:\n${listaPersonasVieja}`);
 
 
-while (seguirModificando) {
-  const nombreModificado = prompt("Ingresa el nombre de la persona a modificar:");
-  const personaAModificar = buscarPersonaPorNombre(nombreModificado);
 
-  if (personaAModificar) {
-    let nuevaEdad = parseInt(prompt(`Ingresa la nueva edad para ${personaAModificar.nombre}:`));
-
-    while (isNaN(nuevaEdad)) {
-      nuevaEdad = parseInt(prompt("No ingresó un número, ingresa la nueva edad:"));
-    }
-
-    personaAModificar.modificarEdad(nuevaEdad);
-    alert(`Edad de ${personaAModificar.nombre} modificada a ${nuevaEdad} años.`);
-
-  } else {
-    alert("No se encontró a la persona a modificar.");
-    seguirModificando = false;
-  }
-
-  const continuarModificando = confirm("¿Quieres seguir modificando?");
-  seguirModificando = continuarModificando;
-}
-
-let listaPersonasNueva = "";
-
-for (const persona of personas) {
-  listaPersonasNueva += `${persona.nombre} (Edad: ${persona.edad})\n`;
-}
-
-console.log(`Lista de personas:\n${listaPersonasNueva}`);
-
-const fechaActual = new Date();
-console.log("Fecha actual:", fechaActual);
-
-const anio = fechaActual.getFullYear();
-console.log("Año actual:", anio);
-
-const mes = fechaActual.getMonth() + 1;
-console.log("Mes actual:", mes);
-
-const dia = fechaActual.getDate();
-console.log("Día actual:", dia);
-
-alert("Fin");
